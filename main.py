a13f5e921de97ff4e74d152294fa887403fc0122
Gokhan <129348617+goksey5@users.noreply.github.com>
2025-06-12 19:22:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
-meme_dict = {
-            "CRINGE": "Garip ya da utandırıcı bir şey",
-            "LOL": "Komik bir şeye verilen cevap",
-            }
-
-word = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
+import discord
+from discord.ext import commands
+from config import TOKEN
 
 
-if word in meme_dict.keys():
-    # Kelime eşleşiyorsa ne yapmalıyız?
-    print(meme_dict[word])
-else:
-    # Kelime eşleşmiyorsa ne yapmalıyız?
-     print("Henüz bu kelimeye sahip değiliz... Ama üzerinde çalışıyoruz!")
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} olarak giriş yaptık')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Merhaba! Ben {bot.user}, bir Discord sohbet botuyum!')
+
+
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+    
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+bot.run(TOKEN)
